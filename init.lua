a1fef214ca66edb235d6a431ad215de3d115b4a0
snowbawler <sudonne@gmail.com>
2025-07-16 23:15:27 -0500

    harpoon and oil

--- init.lua
+++ init.lua
@@ -996,11 +996,13 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.smear_cursor',
   require 'kickstart.plugins.neoscroll',
+  require 'kickstart.plugins.harpoon',
+  require 'kickstart.plugins.oil',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
